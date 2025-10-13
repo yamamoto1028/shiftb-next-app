f12b02ce7e87ad5021c59438a38234ab913f2f6c
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2025-10-13 19:19:06 +0900

    🏷️ 型定義の変更

---
+++ src/app/_types/MicroCmsPost.ts
@@ -0,0 +1,8 @@
+export type MicroCmsPost = {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  categories: { id: string; name: string }[];
+  thumbnail: { url: string; height: number; width: number };
+};
--- src/app/_types/types.ts
+++ src/app/_types/types.ts
@@ -1,10 +1,10 @@
-export type ArticleType = {
-  id: number;
-  title: string;
-  thumbnailUrl: string;
-  createdAt: string;
-  categories: string[];
-  content: string;
-};
+// export type ArticleType = {
+//   id: number;
+//   title: string;
+//   thumbnailUrl: string;
+//   createdAt: string;
+//   categories: string[];
+//   content: string;
+// };
 
-export type ArticleTypes = ArticleType[];
+// export type ArticleTypes = ArticleType[];
