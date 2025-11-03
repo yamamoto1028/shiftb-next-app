f78ffcc086c5a7d9f7476f260727e2b7bba7fb3f
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2025-11-03 20:55:12 +0900

    🔥 不使用ファイルの削除

--- src/app/_types/MicroCmsPost.ts
+++
@@ -1,9 +0,0 @@
-// export type MicroCmsPost = {
-//   id: string;
-//   title: string;
-//   content: string;
-//   thumbnailUrl: { url: string; height: number; width: number };
-//   createdAt: string;
-//   updatedAt: string;
-//   postCategories: { id: string; name: string }[];
-// };
