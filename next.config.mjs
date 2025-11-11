672eb2d6a649a19fbe88408a71e2982170b825a9
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2025-11-12 00:09:21 +0900

    🔧 画像アップロード時の設定更新など

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "placehold.jp" },
-      { protocol: "https", hostname: "images.microcms-assets.io" },
+      {
+        protocol: "https",
+        hostname: "hyfeaseyueuhknopsqpk.supabase.co",
+        pathname: "storage/v1/object/public/**",
+      },
+      { protocol: "https", hostname: "hyfeaseyueuhknopsqpk.supabase.co" },
     ],
   },
 };
